f63bb0b3e6f039ffcf8db2f3eb617944c1e12014
Cyntia Lemus <leon.lemus.cyntia@gmail.com>
2024-03-21 22:54:14 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -26,6 +26,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
+    
     path("posts/<int:pk>/", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>/", UserDetail.as_view(), name="api_user_detail"),
     path("auth/", include("rest_framework.urls")),
@@ -36,6 +37,11 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("", include(router.urls)),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
